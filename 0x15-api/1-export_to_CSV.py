f1bef037cf86e1d7897645ded240f22c53af29ac
root <saheedolabanji@gmail.com>
2023-04-24 09:46:29 -0700

    updated task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees and exporting data in CSV format"""
-import csv
+"""Assessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
@@ -11,14 +11,13 @@ if __name__ == '__main__':
     url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
-    employeeName = response.json().get('name')
+    username = response.json().get('username')
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
     tasks = response.json()
 
-
-    with open(f"{employeeId}.csv", mode='w', newline='') as csv_file:
+    with open('{}.csv'.format(employeeId), 'w') as file:
         for task in tasks:
             file.write('"{}","{}","{}","{}"\n'
                        .format(employeeId, username, task.get('completed'),
