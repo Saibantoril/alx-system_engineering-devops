5f57c79fb6090be38fabd4d804116036905be5e6
root <saheedolabanji@gmail.com>
2023-05-18 11:13:48 +0000

    updated task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 #Increase the amount of traffic an Nginx server can handle
-# Restarts Ngninx
 
 file { '/etc/default/nginx':
   ensure  => file,
   content => template('your_module/nginx_default.erb'),
   notify  => Exec['nginx-restart'],
 }
-
+#restarts Nginx
 exec { 'nginx-restart':
   command     => 'service nginx restart',
   refreshonly => true,
